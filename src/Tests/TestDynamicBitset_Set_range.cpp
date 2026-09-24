6e0069d309bec9f2b5461fd4feedfb1fb50f2e87
athelll <felix.okoronkwo.1@gmail.com>
2026-09-24 16:48:17 +0100

    Dyanamic BItset Layer now working enoug to support Transfer manager layer, set_bits range interface working, test, none and all, find_first and find_next, clear and decode and payload now working, bitset even makes sure that provided bytes as payload has the correct number of end bits padded if there should be any depending on the bit length the dynamic itset was constructed with, if bit length does have its ending bits padded decode failes and returns false tested with numeroust byte sequences as payload to enusre this claim is valid, layer is now valid enough to support Transfer manager rarest first algorithm

--- src/Tests/TestDynamicBitset_Set_range.cpp
+++ src/Tests/TestDynamicBitset_Set_range.cpp
@@ -7,13 +7,25 @@
 int main() {
 
   DynamicBitset set (10);
-  std::array<std::uint8_t, 2> payload {0XAF, 0XC0};
+  std::array<std::uint8_t, 2> payload {0X2F, 0XC0};
 
   auto success = set.decode_as_payload( payload );
   std::cout << std::boolalpha << success << '\n';
 
+  std::cout << "all: " << set.all() << '\n';
+  std::cout << "any: "<< set.any() << '\n';
+  std::cout << "none: " << set.none() << '\n';
+  std::cout << "count: " << set.count() << '\n';
+
+  std::cout << "First set index = " << set.find_first() << '\n';
+  std::cout << "First set index = " << set.find_next(set.find_first()) << '\n';
+
   for (auto index : set.set_bits()) {
     std::cout << "bit " << index << " is set\n";
   }
+
+  set.clear();
+  std::cout << "any: "<< set.any() << '\n';
+
 
 }
